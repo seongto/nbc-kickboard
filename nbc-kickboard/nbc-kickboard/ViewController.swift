bbe5954580ac2fb1f5de6aa17ed8043705b72748
seongto <imseongto@gmail.com>
2024-12-13 16:21:09 +0900

    Project Setup

---
+++ nbc-kickboard/nbc-kickboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nbc-kickboard
+//
+//  Created by MaxBook on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
