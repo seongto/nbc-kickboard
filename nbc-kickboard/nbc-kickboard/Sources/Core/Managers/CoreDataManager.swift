cf0e50f2e862a782c116d4d0640de5ef7eb9852e
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-12-17 11:10:39 +0900

    [Feat] CoreDataManager 추가 (#7)

---
+++ nbc-kickboard/nbc-kickboard/Sources/Core/Managers/CoreDataManager.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataManager.swift
+//  nbc-kickboard
+//
+//  Created by Neoself on 12/17/24.
+//
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    static let shared = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "nbc_kickboard")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print("Error saving context: \(error)")
+            }
+        }
+    }
+}
