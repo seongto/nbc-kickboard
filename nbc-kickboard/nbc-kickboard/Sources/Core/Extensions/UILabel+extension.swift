cbeb923818b9ab1a7431181db233f85544991c37
seongto <imseongto@gmail.com>
2024-12-19 17:12:13 +0900

    [Feat] 모달뷰 컨트롤러 구현 완료 및 로그인, 가입 경고창 UI완성 외
    - 모달뷰의 하위뷰에 대해 프로토콜을 통해 필수 형태 제약을 검.
    - 피그마 디자인을 반영한 알림창 구현
    - 알림창에 후행클로져를 삽입해 필요한 기능을 수행할 수 있도록 함.
    - 기존 기본 알림창을 모두 디자인된 알림창으로 변경.
    - UILabel extension 에 줄간격을 조절가능한 메소드 추가.

--- nbc-kickboard/nbc-kickboard/Sources/Core/Extensions/UILabel+extension.swift
+++ nbc-kickboard/nbc-kickboard/Sources/Core/Extensions/UILabel+extension.swift
@@ -21,7 +21,7 @@ extension UILabel {
         }
     }
     
-    // input 위에 input 설명용 라벨
+    /// input 위에 input 설명용 라벨
     func applyInputLabelStyle(text: String) {
         self.text = text
         self.backgroundColor = .clear
@@ -29,5 +29,68 @@ extension UILabel {
         self.font = Fonts.cap
         self.textColor = Colors.gray1
         self.numberOfLines = 0
+    }
+    
+    /// 알림창 제목 스타일
+    /// - Parameter text: 제목 텍스트
+    func applyAlertTitleLabel(text: String) {
+        self.text = text
+        self.backgroundColor = .clear
+        self.textAlignment = .center
+        self.font = Fonts.subtitleBold
+        self.textColor = Colors.main
+        self.numberOfLines = 1
+    }
+    
+    /// 알림창 메시지 스타일
+    /// - Parameter text: 메시지 텍스트
+    func applyAlertMessageLabel(text: String) {
+        self.text = text
+        self.backgroundColor = .clear
+        self.textAlignment = .left
+        self.font = Fonts.body
+        self.textColor = Colors.black
+        self.numberOfLines = 0
+    }
+}
+
+
+// MARK: - UILabel Utilities
+
+extension UILabel {
+    /// UILabel 에 줄간격을 쉽게 적용시켜 사용하기 위한 메소드
+    /// 기본값을 설정하였으므로 아래 둘 중 하나를 선택하여 사용.
+    /// - Parameters:
+    ///   - lineSpacing: 각 줄 사이의 간격을 의미합니다.
+    ///   - paragraphSpacing: 문단 간격을 설정합니다.
+    func setLineSpacing(lineSpacing: CGFloat = 0.0, paragraphSpacing:CGFloat = 0.0) {
+        guard let labelText = self.text else { return }
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.paragraphSpacing = paragraphSpacing
+
+        let attributedString:NSMutableAttributedString
+        
+        if let labelattributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelattributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+
+        // Line spacing attribute
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
+
+        self.attributedText = attributedString
+        
+        /*
+         위 메소드는 다음과 같은 형식으로 사용합니다.
+         let label = UILabel()
+         
+         label.setLineSpacing(lineSpacing: 10.0)
+         label.setLineSpacing(paragraphSpacing: 10.0)
+         label.setLineSpacing(lineSpacing: 10.0, paragraphSpacing: 10.0)
+         */
     }
 }
